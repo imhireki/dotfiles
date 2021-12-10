d5f2e37bd3680bfae1e49338a64014d6525b3abf
imHireki <imhireki@gmail.com>
2021-12-09 22:10:24 -0300

    feat: add current qtile config:

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,30 +1,4 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from typing import List  # noqa: F401
+from typing import List
 
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
@@ -35,9 +9,6 @@ mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
@@ -58,19 +29,12 @@ keys = [
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "i", lazy.layout.grow()),
+    Key([mod], "m", lazy.layout.shrink()),
+    Key([mod], "n", lazy.layout.normalize()),
+    Key([mod], "o", lazy.layout.maximize()),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
     # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
@@ -79,13 +43,20 @@ keys = [
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
 
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(),
         desc="Spawn a command using a prompt widget"),
 ]
 
-groups = [Group(i) for i in "123456789"]
+groups = [
+    Group(name=n, label=l)
+    for n, l in
+    [
+        (str(n), l)
+        for n,l in enumerate(['一', '二', '三', '四', '五'], 1)
+    ]
+]
 
 for i in groups:
     keys.extend([
@@ -96,61 +67,102 @@ for i in groups:
         # mod1 + shift + letter of group = switch to & move focused window to group
         Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
             desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
     ])
 
 layouts = [
-    layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
+    layout.MonadTall(margin=8,
+                     border_width=2,
+                     border_normal='#D39CDE',
+                     border_focus='#608BDF',
+                     ratio=0.6),
     layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Floating(),
 ]
 
 widget_defaults = dict(
-    font='sans',
-    fontsize=12,
+    font='FiraCode Medium',
+    fontsize=14,
     padding=3,
+    foreground='#000000'
 )
+
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.QuickExit(),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-    ),
-]
+top_bar = bar.Bar(
+    size=20,
+    opacity=0.75,
+    margin=[8, 8, 0, 8],
+    background='#D39CDE',
+    widgets = [
+        widget.TextBox(text=' \uF303',
+                      fontsize='20',
+                      padding=3,
+                      foreground='#000000',
+                      background='#D39CDE',
+                      ),
+
+        widget.TextBox(text='\uE0B8',
+                      fontsize='20',
+                      padding=0,
+                      background='#608BCF',
+                      foreground='#D39CDE',
+                      ),
+
+        widget.CurrentLayout(background='#608BCF'),
+
+        widget.TextBox(text='\uE0B8',
+                      fontsize='20',
+                      padding=0,
+                      foreground='#608BCF',
+                      background='#D39CDE',
+                      ),
+
+        widget.GroupBox(font='FiraCode Bold',
+                        fontsize=16,
+                        highlight_method='text',
+                        active='#608BCF',
+                        inactive='#000000',
+                        this_current_screen_border='#FFFFFF'),
+
+        widget.TextBox(text="\uE0B8",
+                       font="FiraCode",
+                       fontsize="20",
+                       padding=0,
+                       background='#608BDF',
+                       foreground='#D39CDE',
+                       ),
+
+        widget.Prompt(foreground='#000000',
+                      background='#608BDF',
+                      prompt='spell: '),
+
+        widget.WindowName(background='#608BDF'),
+
+        widget.TextBox(text="\uE0B8",
+                       font="FiraCode",
+                       fontsize="20",
+                       padding=0,
+                       foreground='#608BDF',
+                       background='#D39CDE',
+                       ),
+
+        widget.Systray(),
+
+        widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+
+        widget.TextBox(text="\uE0B8",
+                       font="FiraCode",
+                       fontsize="20",
+                       padding=0,
+                       background='#608BDF',
+                       foreground='#D39CDE',
+                       ),
+
+        widget.QuickExit(background='#608BDF'),
+        ])
+
+main_screen = Screen(top=top_bar)
+screens = [main_screen]
 
 # Drag floating layouts.
 mouse = [
@@ -184,12 +196,6 @@ reconfigure_screens = True
 # focus, should we respect this or not?
 auto_minimize = True
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
