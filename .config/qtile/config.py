a7e676e26340470accf416c4839dd42b7d4a769e
imHireki <imhireki@gmail.com>
2021-12-26 10:50:04 -0300

    feat: remake keybindings

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,45 +1,62 @@
 from typing import List
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, widget, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
-from libqtile import qtile
 
-mod = "mod4"
-terminal = guess_terminal()
+M4 = 'mod4'
+M1 = 'mod1'
 
 keys = [
-    # Switch Between Windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move focus next"),
+    # Move window focus
+    Key([M4], "h", lazy.layout.left()),
+    Key([M4], "l", lazy.layout.right()),
+    Key([M4], "j", lazy.layout.down()),
+    Key([M4], "k", lazy.layout.up()),
+    Key([M4], "space", lazy.layout.next()),
 
-    # Move the Windows
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="M/ left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="M/ right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="M/ down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="M/ up"),
+    # Move the window
+    Key([M4, "shift"], "h", lazy.layout.shuffle_left()),
+    Key([M4, "shift"], "l", lazy.layout.shuffle_right()),
+    Key([M4, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([M4, "shift"], "k", lazy.layout.shuffle_up()),
 
     # Resize the Windows
-    Key([mod], "i", lazy.layout.grow()),
-    Key([mod], "m", lazy.layout.shrink()),
-    Key([mod], "n", lazy.layout.normalize()),
-    Key([mod], "o", lazy.layout.maximize()),
+    Key([M4], 'u', lazy.layout.shrink()),
+    Key([M4], 'i', lazy.layout.grow()),
+    Key([M4], "o", lazy.layout.maximize()),
+    Key([M4], "y", lazy.layout.normalize()),
 
-    # Apps
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "r", lazy.spawncmd()),
-    Key([mod], "e", lazy.spawn("emacsclient -c -a 'emacs'")),
+    # Print ( date +  clipboard yank )
+    Key([M4], 'p', lazy.spawn('./scripts/print.sh')),
+    Key([M4, 'shift' ], 'p', lazy.spawn('./scripts/print_select.sh')),
 
     # Functions
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "b", lazy.hide_show_bar()),
+    Key([M4], "q", lazy.window.kill()),
+    Key([M4], "w", lazy.next_layout()),
+    Key([M4], "e", lazy.hide_show_bar()),
+    Key([M4], 'r', lazy.spawncmd()),
+
+    # Apps
+    Key([M1], '1', lazy.spawn("kitty")),
+    Key([M1], '2', lazy.spawn("emacsclient -c -a 'emacs'")),
+    Key([M1], '3', lazy.spawn('librewolf')),
+
+    # Volume
+    Key([M1], 'q', lazy.spawn('amixer -q -D pulse set Master 10%-')),
+    Key([M1], 'w', lazy.spawn('amixer -q -D pulse set Master 10%+')),
+    Key([M1], 'e', lazy.spawn('amixer -q -D pulse set Master toggle')),
+
+    # Scripts
+    Key([M1], 'h', lazy.spawn('sudo ./scripts/clear_drop_caches.sh')),
+
+    # Qtile Managemant
+    Key([M1, 'control'], '1', lazy.restart()),
+    Key([M1, 'control'], '2', lazy.shutdown()),
+
+    # Session
+    Key([M1, 'control'], '3', lazy.spawn('shutdown now')),
+    Key([M1, 'control'], '4', lazy.spawn('reboot')),
 ]
 
 groups = [
@@ -47,19 +64,22 @@ groups = [
     for n, l in
     [
         (str(n), l)
-        for n,l in enumerate(['一', '二', '三', '四', '五'], 1)
+        for n,l in [('a', '一'),
+                    ('s', '二'),
+                    ('d', '三'),
+                    ('f', '四'),
+                    ('g', '五')]
     ]
 ]
 
 for i in groups:
     keys.extend([
         # Switch to a group
-        Key([mod], i.name, lazy.group[i.name].toscreen()),
+        Key([M4], i.name, lazy.group[i.name].toscreen()),
 
         # Move focused to a group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=False)),
+        Key([M4, "shift"], i.name, lazy.window.togroup(i.name, switch_group=False)),
     ])
-
 
 layouts = [
     layout.MonadTall(margin=8,
@@ -70,6 +90,7 @@ layouts = [
     layout.Max(),
     layout.Floating(),
 ]
+
 
 widget_defaults = dict(
     font='FiraCode Medium',
@@ -93,13 +114,14 @@ reverse = {'background' : colors['secondary'],
 textbox = {'fontsize': '20',
            'padding': 0}
 
+
 top_bar = bar.Bar(
     size=20,
     opacity=0.75,
     margin=[8, 8, 0, 8],
     background=colors['primary'],
     widgets = [
-        widget.Image(filename='~/penguin.png'),
+        widget.Image(filename='~/Pictures/icon.png'),
 
         widget.Prompt(foreground=colors['font'],
                       background=colors['primary'],
@@ -122,7 +144,7 @@ top_bar = bar.Bar(
                         active=colors['primary'],
                         ),
 
-        widget.TextBox(text="\uE0B8",
+        widget.TextBox(text="\uE0B0",
                        **textbox,
                        **standard
                        ),
@@ -178,11 +200,11 @@ screens = [main_screen]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
+    Drag([M4], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
+    Drag([M4], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Click([M4], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
@@ -203,11 +225,5 @@ floating_layout = layout.Floating(float_rules=[
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
-
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
