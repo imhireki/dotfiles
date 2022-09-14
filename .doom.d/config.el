c257e3fdcf09b41888b27e882593bc7db38d8776
imHireki <imhireki@gmail.com>
2022-09-14 16:39:24 -0300

    update doom emacs configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,13 +3,14 @@
 (setq doom-theme 'doom-dracula)
 (setq display-line-numbers-type t)
 (setq delete-selection-mode t)
+
 ;;Background Opacity
 ;;(set-frame-parameter (selected-frame) 'alpha '(70 70))
 ;;(add-to-list 'default-frame-alist '(alpha 70 70))
 
 ;; Documents
-(setq org-directory "~/documents/org/")
-(setq fancy-splash-image "~/pictures/splash.png")
+(setq org-directory "~/Documents/org/")
+(setq fancy-splash-image "~/Pictures/random/splash.png")
 
 ;; Hook
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
@@ -21,3 +22,9 @@
   (require 'tree-sitter-langs)
   (global-tree-sitter-mode)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+;; Projectile
+(setq doom-themes-treemacs-theme "doom-colors")
+(setq projectile-project-search-path '("~/Documents/projects/"
+                                       "~/.config/"
+                                       "~/Documents/"))
